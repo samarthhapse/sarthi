e17f46ccba5e05b4cfa5482a3693c5593a6e703f
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-06-05 16:03:16 +0530

    fixed preloader issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import store from './redux/store.js'
+import Preloader from './components/preloader/Preloader.jsx'
 
 import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <Preloader />
   </Provider>
 )
