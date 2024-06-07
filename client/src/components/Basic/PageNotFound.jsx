1727dfd40e428ddece7523cf3197affb4115bea4
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-06-07 14:23:50 +0530

    changes made

--- client/src/components/Basic/PageNotFound.jsx
+++ client/src/components/Basic/PageNotFound.jsx
@@ -4,8 +4,15 @@ import err from '../../assets/err.json'
 function Error(){
     return(
         <div>
-            <div className="flex justify-center relative top-[8.5rem]">
-                  <div className='w-80 h-80'><Lottie animationData={err}/> </div> 
+            <div className="p-0 [overflow-x:hidden] bg-black">
+            {/* <div className='flex justify-center'>
+                        <div><a href='/'><button className='absolute z-10 bg-[#213547] p-2 rounded-md text-white'>Return To Home Page</button> </a> </div>
+                    </div> */}
+                    <div className='flex justify-center items-center w-[100vw] h-[100vh] absolute z-10'>
+                <div><a href='/'><button className='b bg-[#213547]  p-2 rounded-md text-white hover:bg-[white] hover:text-[#213547]'>Return To Home Page</button> </a> </div>
+                </div>
+                  <div><Lottie animationData={err}/> </div> 
+                    
                 </div>   
         </div>
     )
