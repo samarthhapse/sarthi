1bc642efcc5c3cdd8c0a99a9a9164184f923ccc9
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2024-06-09 19:16:30 +0530

    Revert "Modern Landing Page is created."

--- client/src/components/Basic/Navbar.jsx
+++ client/src/components/Basic/Navbar.jsx
@@ -1,128 +1,118 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { FaChevronDown } from "react-icons/fa";
-import { FiBox, FiFilm, FiPenTool } from "react-icons/fi"; // Example icons
-import pic from "/src/assets/react.svg";
+import React, { useState, useContext } from "react";
+import { ThemeContext } from "../context/theme";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+import { useRef } from "react";
+import { useEffect } from "react";
 
-const Navbar = () => {
-  const [isFeaturesOpen, setIsFeaturesOpen] = useState(false);
-  const [isResourcesOpen, setIsResourcesOpen] = useState(false);
+function navLinkClass(isActive, theme) {
+  return clsx(
+    "block mx-auto w-1/2 sm:w-full rounded-lg p-1 px-3 py-2 hover:bg-sky-500 hover:text-xl",
+    theme.background === "#fff" ? "text-black hover:bg-amber-300" : "text-white",
+    {
+      "bg-sky-500": isActive,
+    }
+  );
+}
+
+export default function Navbar() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const [navbarShown, setNavbarShown] = useState(false);
+
+  // To make the navbar (in mobile view) get hidden when the user clicks/scroll outside it...
+  const navbarRef = useRef(null);
+
+  useEffect(() => {
+    function handleMouseDown(event) {
+      if (
+        navbarShown &&
+        navbarRef.current &&
+        !navbarRef.current.contains(event.target)
+      ) {
+        setNavbarShown(!false);
+        console.log("outside");
+      }
+    }
+    function handleScroll() {
+      if (navbarShown) {
+        setNavbarShown(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleMouseDown);
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("mousedown", handleMouseDown);
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [navbarShown]);
+  //
+
+  const toggleNavbar = () => {
+    setNavbarShown(!navbarShown);
+  };
 
   return (
-    <div className="w-full h-10 flex pt-8 m-0 text-xl">
-      <div className="flex justify-center items-center ml-4 mr-[100px]">
-        <div className="w-9 h-9 mx-3">
-          <img src={pic} className="w-full h-full" alt="Logo" />
-        </div>
-        <h3>Welcome</h3>
-      </div>
-      <div className="flex items-center justify-center ml-[100px] w-[800px] mr-[50px]">
-        <div className="relative">
-          <button
-            onClick={() => setIsFeaturesOpen(!isFeaturesOpen)}
-            className="mx-8 flex items-center hover:text-gray-700 transition duration-300"
+    <nav
+      aria-label="Site Nav"
+      className="flex items-center justify-between max-w-8xl p-4 mx-auto sticky top-0 bg-light z-10 border-b drop-shadow-sm"
+    >
+      <a href="/">
+        <img
+          src=""
+          className="inline-flex h-[1] w-10 items-center justify-center rounded-lg" // don't change logo's height and width here
+          alt="Saarthi Logo"
+          loading="lazy"
+        ></img>
+      </a>
+
+      <ul
+        ref={navbarRef}
+        // className='flex flex-wrap items-center justify-center gap-2 text-[1rem]'
+        className={clsx(
+          `fixed sm:static top-20 z-10 gap-2 text-md w-full sm:flex flex-wrap items-center justify-center nav-menu`,
+          theme.background === "#fff" ? "bg-white" : "bg-black",
+          navbarShown ? "navbar-shown" : "navbar-hidden"
+        )}
+        onClick={toggleNavbar}
+      >
+        <li className="mb-4 sm:mb-0 sm:ml-8 nav-item text-center">
+          <NavLink
+            className={({ isActive }) => navLinkClass(isActive, theme)}
+            to="/Login"
           >
-            Features <FaChevronDown className="ml-1 mt-[6px]" />
-          </button>
-          {isFeaturesOpen && (
-            <div className="absolute bg-gray-100 shadow-lg rounded-lg mt-2 p-4 w-64">
-              <div className="hover:bg-gray-400 p-2 cursor-pointer flex items-center">
-                <FiBox className="mr-3" />
-                <div>
-                  <p className="font-bold">Create & Integrate</p>
-                  <p className="text-sm text-gray-600">Option A description</p>
-                </div>
-              </div>
-              <div className="hover:bg-gray-200 p-2 cursor-pointer flex items-center">
-                <FiFilm className="mr-3" />
-                <div>
-                  <p className="font-bold">Welcome Studio</p>
-                  <p className="text-sm text-gray-600">Option B description</p>
-                </div>
-              </div>
-              <div className="hover:bg-gray-200 p-2 cursor-pointer flex items-center">
-                <FiPenTool className="mr-3" />
-                <div>
-                  <p className="font-bold">Content Creator</p>
-                  <p className="text-sm text-gray-600">Option C description</p>
-                </div>
-              </div>
-              <div className="hover:bg-gray-200 p-2 cursor-pointer flex items-center text-center">
-                <div>
-                  <p className="font-bold ml-7">See all Features</p>
-                </div>
-              </div>
-              <div className="hover:bg-gray-200 p-2 cursor-pointer flex items-center">
-                <div>
-                  <p className="font-bold">Take the Interactive Tour</p>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-
-        <Link
-          key="price"
-          to="/pricing"
-          className="mx-10 hover:text-gray-700 transition duration-300"
-        >
-          Pricing
-        </Link>
-
-        <div className="relative z-40">
-          <button
-            onClick={() => setIsResourcesOpen(!isResourcesOpen)}
-            className="mx-6 flex items-center hover:text-gray-700 transition duration-300"
-          >
-            Resources <FaChevronDown className="ml-1 mt-[6px]" />
-          </button>
-          {isResourcesOpen && (
-            <div className="absolute bg-pink-100  shadow-lg rounded-lg mt-2 p-4 w-64">
-              <div className="hover:bg-gray-200 p-2 cursor-pointer flex items-center">
-                <FiBox className="mr-3" />
-                <div>
-                  <p className="font-bold">Option A</p>
-                  <p className="text-sm text-gray-600">Description A</p>
-                </div>
-              </div>
-              <div className="hover:bg-gray-200 p-2 cursor-pointer flex items-center">
-                <FiFilm className="mr-3" />
-                <div>
-                  <p className="font-bold">Option B</p>
-                  <p className="text-sm text-gray-600">Description B</p>
-                </div>
-              </div>
-              <div className="hover:bg-gray-200 p-2 cursor-pointer flex items-center">
-                <FiPenTool className="mr-3" />
-                <div>
-                  <p className="font-bold">Option C</p>
-                  <p className="text-sm text-gray-600">Description C</p>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-
-        <Link
-          key="events"
-          to="/events"
-          className="mx-6 hover:text-gray-700 transition duration-300"
-        >
-          Events
-        </Link>
-      </div>
-      <div className="flex justify-center items-center ml-9">
-        <Link to="/studentsignup">
-          <button className="hover:text-gray-700 transition duration-300">
             Login
-          </button>
-        </Link>
-        <button className="bg-black w-[120px] mx-6 text-[16px] text-base h-8 rounded-2xl text-white hover:bg-slate-500 transition duration-300">
-          Book a demo
-        </button>
-      </div>
-    </div>
-  );
-};
+          </NavLink>
+        </li>
+      </ul>
+      <button onClick={toggleTheme} className="text-2xl">
+        {theme.icon}
+      </button>
 
-export default Navbar;
+      <div
+        className={clsx("hamburger", navbarShown && "active")}
+        onClick={toggleNavbar}
+      >
+        <span
+          className={clsx(
+            "bar",
+            theme.background === "#fff" ? "bg-black" : "bg-white"
+          )}
+        ></span>
+        <span
+          className={clsx(
+            "bar",
+            theme.background === "#fff" ? "bg-black" : "bg-white"
+          )}
+        ></span>
+        <span
+          className={clsx(
+            "bar",
+            theme.background === "#fff" ? "bg-black" : "bg-white"
+          )}
+        ></span>
+      </div>
+    </nav>
+  );
+}
