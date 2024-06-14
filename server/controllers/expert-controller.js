aab8a6639ad0f9eb8ded170a083e482bfb576b17
chetan <jellybeans33124@gmail.com>
2024-06-14 20:32:51 +0530

    cloudinary setup

--- server/controllers/expert-controller.js
+++ server/controllers/expert-controller.js
@@ -1,161 +1,229 @@
 import { Expert } from "../models/expert-model.js";
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { Student } from "../models/student-model.js";
 import OTP from "../models/otp-model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 export const register = async (req, res) => {
-    
-     try {
-        let user,checkStudent;
-        const { name, email, phoneNo, expertise, field, jobTitle, password, confirmPassword,otp} = req.body;
-        if (!name || !password || !confirmPassword || !phoneNo || !expertise || !field || !jobTitle) {
-            return res.status(400).json({ message: "All fields are required", success: false });
-        }
-        if (password !== confirmPassword) {
-            return res.status(400).json({ message: "Password do not match", success: false });
-        }
-        user = await Expert.findOne({ email });
-        checkStudent=await Student.findOne({email});
-        if (user || checkStudent) {
-            return res.status(400).json({ message: "Email already exist, try different email", success: false });
-        }
+  try {
+    console.log("request", req.files);
+    let user, checkStudent;
+    const {
+      name,
+      email,
+      phoneNo,
+      expertise,
+      field,
+      jobTitle,
+      password,
+      confirmPassword,
+      otp,
+    } = req.body;
+    if (
+      !name ||
+      !password ||
+      !confirmPassword ||
+      !phoneNo ||
+      !expertise ||
+      !field ||
+      !jobTitle
+    ) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
+    }
+    if (password !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ message: "Password do not match", success: false });
+    }
+    user = await Expert.findOne({ email });
+    checkStudent = await Student.findOne({ email });
+    if (user || checkStudent) {
+      return res.status(400).json({
+        message: "Email already exist, try different email",
+        success: false,
+      });
+    }
 
-        const recentOtp = await OTP.findOne({ email })
-        .sort({ createdAt: -1 })
-        .limit(1);
-        if (recentOtp.length === 0) {
-           return res.status(400).json({
-             success: false,
-             message: "OTP Not Found!",
-           });
-         }
-         if (otp !== recentOtp.otp) {
-           return res.status(400).json({
-             success: false,
-             message: "Invalid OTP",
-           });
-         }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        user = await Expert.create({
-            name,
-            email,
-            phoneNo,
-            password: hashedPassword,
-            expertise,
-            field,
-            jobTitle
-        })
-        if (!user) {
-            return res.status(500).json({ message: "internal server error", success: false })
-        }
-        return res.status(201).json({
-            message: "Account created successfully.",
-            success: true
-        })
+    const recentOtp = await OTP.findOne({ email })
+      .sort({ createdAt: -1 })
+      .limit(1);
+    if (recentOtp.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "OTP Not Found!",
+      });
     }
-    catch (err) {
-        return res.status(500).json({ message: "internal server error", err, success: false })
+    if (otp !== recentOtp.otp) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP",
+      });
     }
-}
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    if (!avatarLocalPath) {
+      console.log("avatarLocalPath not found");
+      return res.status(401).json({
+        success: false,
+        message: "Avatar is required",
+      });
+    }
+    console.log(avatarLocalPath);
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    if (!avatar) {
+      console.log("avatar not uploaded on cloudinary");
+
+      return res.status(401).json({
+        success: false,
+        message: "Avatar is required",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = await Expert.create({
+      name,
+      email,
+      phoneNo,
+      password: hashedPassword,
+      expertise,
+      field,
+      jobTitle,
+      avatar: avatar.url,
+    });
+    if (!user) {
+      console.log("user not created in database");
+      return res
+        .status(500)
+        .json({ message: "internal server error", success: false });
+    }
+    return res.status(201).json({
+      message: "Account created successfully.",
+      success: true,
+    });
+  } catch (err) {
+    console.log("error while registering", err);
+    return res
+      .status(500)
+      .json({ message: "internal server error", err, success: false });
+  }
+};
 
 export const login = async (req, res) => {
-
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(400).json({ message: "All fields are required", success: false });
-        };
-        const user = await Expert.findOne({ email });
-        if (!user) {
-            return res.status(400).json({
-                message: "Incorrect username or password",
-                success: false
-            })
-        };
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if (!isPasswordMatch) {
-            return res.status(400).json({
-                message: "Incorrect username or password",
-                success: false
-            })
-        };
-        const tokenData = {
-            userId: user._id
-        };
-
-        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
-
-        if (!token) {
-            return res.status(500).json({ message: "internal server error", success: false })
-        }
-        return res.status(200).json({ token: token,message:"logged in successful.", success: true })
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
     }
-    catch (err) {
-        return res.status(500).json({ message: "internal server error", err, success: false })
+    const user = await Expert.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Incorrect username or password",
+        success: false,
+      });
     }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Incorrect username or password",
+        success: false,
+      });
+    }
+    const tokenData = {
+      userId: user._id,
+    };
 
-}
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    if (!token) {
+      return res
+        .status(500)
+        .json({ message: "internal server error", success: false });
+    }
+    return res
+      .status(200)
+      .json({ token: token, message: "logged in successful.", success: true });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "internal server error", err, success: false });
+  }
+};
 
 export const changePassword = async (req, res) => {
+  try {
+    const { email, current_password, new_password, confirm_new_password } =
+      req.body;
+    if (new_password !== confirm_new_password) {
+      return res.status(400).json({
+        message: "Confirm password do not match.",
+        success: false,
+      });
+    }
+    const user = await Expert.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "No user found!", success: false });
+    }
+    const isPasswordMatch = await bcrypt.compare(
+      current_password,
+      user.password
+    );
+    if (!isPasswordMatch)
+      return res.status(400).json({
+        message: "Current password is incorrect.",
+        success: false,
+      });
+    const new_hash_pass = await bcrypt.hash(new_password, 10);
+    user.password = new_hash_pass;
+    await user.save();
 
-    try {
-        const { email, current_password, new_password,confirm_new_password } = req.body;
-        if(new_password !== confirm_new_password)
-            {
-                return res.status(400).json({
-                    message: "Confirm password do not match.",
-                    success: false
-                })
-            }
-        const user = await Expert.findOne({ email })
-        if (!user) {
-            return res.status(400).json({ message: "No user found!", success: false });
-        }
-        const isPasswordMatch = await bcrypt.compare(current_password, user.password);
-        if (!isPasswordMatch)
-            return res.status(400).json({
-                message: "Current password is incorrect.",
-                success: false
-            })
-        const new_hash_pass = await bcrypt.hash(new_password, 10);
-        user.password = new_hash_pass
-        await user.save()
+    return res
+      .status(200)
+      .json({ message: "password changed successfully.", success: true });
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ message: "internal server error", err, success: false });
+  }
+};
 
-        return res.status(200).json({ message: "password changed successfully.", success: true })
+export const expertDetails = async (req, res) => {
+  const userID = req.params.id;
+  try {
+    const user = await Expert.findById(userID).select("-password");
+    if (!user) {
+      return res
+        .status(500)
+        .json({ message: "internal server error", err, success: false });
     }
-    catch (err) {
-        return res.status(400).json({ message: "internal server error", err, success: false })
-    }
-}
+    return res.status(200).json({ userDetails: user, success: true });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "internal server error", err, success: false });
+  }
+};
 
-export const expertDetails=async(req,res)=>{
-    const userID=req.params.id;
-    try{
-    const user=await Expert.findById(userID).select('-password')
-    if(!user)
-       {
-          return res.status(500).json({ message: "internal server error", err, success: false })
-       }
-       return res.status(200).json({ userDetails:user,success: true })
+export const getAllExperts = async (req, res) => {
+  try {
+    const user = await Expert.find().select("-password");
+    if (!user) {
+      return res
+        .status(500)
+        .json({ message: "internal server error", err, success: false });
     }
-    catch(err)
-    {
-       return res.status(500).json({ message: "internal server error", err, success: false })
-    }
- }
-
- export const getAllExperts=async(req,res)=>{
-    try{
-      const user=await Expert.find().select("-password")
-      if(!user)
-       {
-          return res.status(500).json({ message: "internal server error", err, success: false })
-       }
-       return res.status(200).json({user})
-    }
-    catch(err)
-    {
-       return res.status(500).json({ message: "internal server error", err, success: false })
-    }
- } 
+    return res.status(200).json({ user });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "internal server error", err, success: false });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
-import mongoose from "mongoose";
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/connection.js";
-import studentRoute from './routes/studentRoute.js';
-import expertRoute from './routes/expertRoute.js';
-import otpRoute from './routes/otpRoute.js'
+import studentRoute from "./routes/studentRoute.js";
+import expertRoute from "./routes/expertRoute.js";
+import otpRoute from "./routes/otpRoute.js";
 dotenv.config({});
 
-const app=express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use('/api/v1/student/',studentRoute)
-app.use('/api/v1/expert/',expertRoute)
-app.use('/api/v1/otp/',otpRoute)
+app.use("/api/v1/student/", studentRoute);
+app.use("/api/v1/expert/", expertRoute);
+app.use("/api/v1/otp/", otpRoute);
 app.listen(PORT, () => {
-    connectDB()
-        .then(() => {
-            console.log(`Server Listening on : http://localhost:${PORT}`)
-        })
-})
+  connectDB().then(() => {
+    console.log(`Server Listening on : http://localhost:${PORT}`);
+  });
+});
