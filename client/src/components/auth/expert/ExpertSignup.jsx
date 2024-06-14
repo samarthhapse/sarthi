0221dc70f9306769a92f6c95da46e067e3c1accd
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2024-06-14 16:49:22 +0530

    Now dark and light theme has been integrated

--- client/src/components/auth/expert/ExpertSignup.jsx
+++ client/src/components/auth/expert/ExpertSignup.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { motion } from "framer-motion";
-
+import { useTheme } from "../../providers/ThemeProvider";
 const ExpertSignup = () => {
+  const { isDarkMode } = useTheme();
   const navigate = useNavigate();
   const [inputs, setInputs] = useState({
     name: '',
@@ -46,15 +47,15 @@ const ExpertSignup = () => {
   };
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center p-5 bg-cover bg-center bg-custom-gradient text-white">
+    <div className={`w-full min-h-screen flex items-center justify-center p-4 bg-cover bg-center ${isDarkMode ? 'bg-custom-gradient text-white' :' bg-white '} `} >
       <motion.div 
-         className="w-full md:w-[900px] flex flex-col md:flex-row rounded-lg shadow-lg overflow-hidden"
+        className="w-[900px] flex rounded-lg shadow-lg overflow-hidden"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
        
-        <div className="md:flex-[1.5] flex flex-col items-center justify-center bg-white p-10 bg-card-custom-gradient text-white">
+       <div className={ `flex-[1.5] flex flex-col  p-10 ${isDarkMode ? ' bg-card-custom-gradient ' : ' bg-teal-500 text-white' }` }>
           <motion.form 
             onSubmit={handleSubmit} 
             className="flex flex-col items-center w-full"
@@ -70,8 +71,8 @@ const ExpertSignup = () => {
               onChange={handleChange}
               value={inputs.name}
               required
-              className="w-full md:w-[370px] py-4 px-6 mb-8 mt-10 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
-              />
+              className="w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+            />
             <input
               type="email"
               placeholder="Email"
@@ -79,8 +80,8 @@ const ExpertSignup = () => {
               onChange={handleChange}
               value={inputs.email}
               required
-              className="w-full md:w-[370px] py-4 px-6 mb-8 mt-10 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
-        />
+              className="w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+            />
             <input
               type="text"
               placeholder="Phone number"
@@ -88,8 +89,8 @@ const ExpertSignup = () => {
               onChange={handleChange}
               value={inputs.phoneNo}
               required
-              className="w-full md:w-[370px] py-4 px-6 mb-8 mt-10 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
-              />
+              className="w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+            />
             <input
               type="text"
               placeholder="Field"
@@ -97,8 +98,8 @@ const ExpertSignup = () => {
               onChange={handleChange}
               value={inputs.field}
               required
-              className="w-full md:w-[370px] py-4 px-6 mb-8 mt-10 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
-        />
+              className="w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+            />
             <input
               type="text"
               placeholder="Job title"
@@ -106,8 +107,8 @@ const ExpertSignup = () => {
               onChange={handleChange}
               value={inputs.jobTitle}
               required
-              className="w-full md:w-[370px] py-4 px-6 mb-8 mt-10 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
-        />
+              className="w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+            />
             <input
               type="password"
               placeholder="Password"
@@ -115,8 +116,8 @@ const ExpertSignup = () => {
               onChange={handleChange}
               value={inputs.password}
               required
-              className="w-full md:w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
-        />
+              className="w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+            />
             <input
               type="password"
               placeholder="Confirm password"
@@ -124,13 +125,12 @@ const ExpertSignup = () => {
               onChange={handleChange}
               value={inputs.confirmPassword}
               required
-              className="w-full md:w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
-              />
+              className="w-[370px] py-4 px-6 mb-4 text-sm bg-gray-100 border border-gray-300 rounded-lg outline-none transition-all focus:border-teal-500 focus:ring focus:ring-teal-500 focus:ring-opacity-50"
+            />
             <div className="mb-4 mr-32">
               <div className="mr-40">
-                <h3 className="text-md text-gray-700 font-md mb-2">Expertise:</h3>
+                <h3 className="text-md font-md mb-2">Expertise:</h3>
               </div>
-              
               <div className="flex flex-col">
                 <div className="flex">
                   <input
@@ -142,9 +142,9 @@ const ExpertSignup = () => {
                     checked={inputs.expertise === 'Bug solving'}
                     className="mr-2"
                   />
-                  <label htmlFor="bugSolving" className="mr-4 text-gray-700">Bug solving</label>
+                  <label htmlFor="bugSolving" className="mr-4">Bug solving</label>
                 </div>
-                <div className=" flex">
+                <div className="flex">
                   <input
                     type="radio"
                     id="techCareer"
@@ -154,7 +154,7 @@ const ExpertSignup = () => {
                     checked={inputs.expertise === 'Tech career assistance'}
                     className="mr-2"
                   />
-                  <label htmlFor="techCareer" className="mr-4  text-gray-700 ">Tech career assistance</label>
+                  <label htmlFor="techCareer" className="mr-4">Tech career assistance</label>
                 </div>
                 <div className="flex">
                   <input
@@ -166,7 +166,7 @@ const ExpertSignup = () => {
                     checked={inputs.expertise === 'Academic support'}
                     className="mr-2"
                   />
-                  <label htmlFor="academicSupport" className="mr-4  text-gray-700">Academic support</label>
+                  <label htmlFor="academicSupport">Academic support</label>
                 </div>
               </div>
             </div>
@@ -180,7 +180,7 @@ const ExpertSignup = () => {
             </motion.button>
           </motion.form>
         </div>
-        <div className="md:w-[50%] flex flex-col items-center justify-center bg-teal-500 p-3 bg-card-custom-gradient text-white">
+        <div className={`flex-1 flex flex-col items-center justify-center bg-card-custom-gradient p-3 ${isDarkMode ? 'bg-card-custom-gradient' :' bg-teal-500 text-white'}`}>
           <h1 className="text-white text-2xl font-[serif]">Already a registered expert?</h1>
           <Link to='/expertlogin'>
             <motion.button 
---
+++ client/src/components/providers/ThemeProvider.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react-refresh/only-export-components */
+
+
+
+import  { createContext, useState, useContext } from "react";
+
+
+const ThemeContext = createContext();
+
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
