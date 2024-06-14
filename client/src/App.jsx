f9222076aaae411bc5f0c38ebaa5b4213f447027
chetan <jellybeans33124@gmail.com>
2024-06-14 10:08:00 +0530

    add otpverifyexpert path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./components/context/theme";
@@ -24,25 +24,26 @@ function App() {
   // const { theme } = useContext(ThemeContext);
   return (
     <ThemeProvider>
-    <BrowserRouter>
-      <Navbar />
-      <Routes >
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Landing" element={<Landing />} />
-        <Route path="/studenthome" element={<StudentHome />} />
-        <Route path="/experthome" element={<ExpertHome />} />
-        <Route path="/studentsignup" element={<StudentSignup />} />
-        <Route path="/studentlogin" element={<StudentLogin />} />
-        <Route path="/studentforget" element={<StudentForget />} />
-        <Route path="/expertsignup" element={<ExpertSignup />} />
-        <Route path="/expertlogin" element={<ExpertLogin />} />
-        <Route path="/expertforget" element={<ExpertForget />} />
-        <Route path="/otpverifystudent" element={<OtpVerifyStudent />} />
-        <Route path="/events" element={<OtpVerifyExpert />} />
-        <Route path="/pricing" element={<OtpVerifyExpert />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Landing" element={<Landing />} />
+          <Route path="/studenthome" element={<StudentHome />} />
+          <Route path="/experthome" element={<ExpertHome />} />
+          <Route path="/studentsignup" element={<StudentSignup />} />
+          <Route path="/studentlogin" element={<StudentLogin />} />
+          <Route path="/studentforget" element={<StudentForget />} />
+          <Route path="/expertsignup" element={<ExpertSignup />} />
+          <Route path="/expertlogin" element={<ExpertLogin />} />
+          <Route path="/expertforget" element={<ExpertForget />} />
+          <Route path="/otpverifystudent" element={<OtpVerifyStudent />} />
+          <Route path="/otpverifyexpert" element={<OtpVerifyExpert />} />
+          <Route path="/events" element={<HomePage />} />
+          <Route path="/pricing" element={<HomePage />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
