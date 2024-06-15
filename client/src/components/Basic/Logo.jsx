dd1c36a94bcc815c5f3d417daf1c2a243be63f9a
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2024-06-15 19:40:08 +0530

    Update Logo.jsx

--- client/src/components/Basic/Logo.jsx
+++ client/src/components/Basic/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = () => {
   return (
     <Link to="/" className="flex items-center ml-4 md:ml-8 hover:text-gray-700 transition duration-300 transform hover:scale-105">
       <h3 className={`font-bold text-3xl ${isDarkMode ? 'text-white' : 'text-blue-900'}`}>
-        SARTHI
+        sarthi
       </h3>
     </Link>
   );
